078c408cb737bbd21df77dee68592dcb662d88ea
confusedalex <alex@confusedalex.dev>
2025-01-17 22:18:07 +0100

    feat: add installation guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,30 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 // https://astro.build/config
 export default defineConfig({
 	site: "https://thegoldeconomy.confusedalex.dev",
 	integrations: [
 		starlight({
 			title: 'TheGoldEconomy',
+			description: 'TheGoldEconomy is a simple, easy to use, and powerful economy plugin for Minecraft servers.',
+			editLink: {
+				baseUrl: 'https://github.com/confusedalex/goldeconomy-docs/edit/main/',
+			},
 			logo: {
 				src: './src/assets/logo.svg',
 				replacesTitle: true,
 			},
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/confusedalex/goldeconomy',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
+					label: 'Start Here',
+					autogenerate: { directory: 'start-here' }
+				}
 			],
 		}),
 	],
+
 });
